2fed647b6e2642f4ad418daa426b71f0a2937483
sbetiin_@hotmail.com <sbetiin_@hotmail.com>
2024-01-12 22:02:43 -0300

    Create mentoria e curso

--- src/main/java/one/digitalinnovation/Main.java
+++ src/main/java/one/digitalinnovation/Main.java
@@ -1,7 +1,32 @@
 package one.digitalinnovation;
 
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Colections");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Introdução");
+        curso2.setCargaHoraria(11);
+
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
